620c64bd89e20e8930a54fde9e8c117e2f5a9556
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2021-12-06 19:47:04 +0300

    add chart

--- ExchangeRates/View Controllers/CurrencyViewController.swift
+++ ExchangeRates/View Controllers/CurrencyViewController.swift
@@ -12,11 +12,6 @@ import Charts
 
 class CurrencyViewController: UIViewController {
     
-    lazy var lineChartView: LineChartView = {
-        let chartView = LineChartView()
-        chartView.backgroundColor = .systemRed
-        return chartView
-    }()
     
     @IBOutlet weak var label1: UILabel!
     
@@ -32,10 +27,39 @@ class CurrencyViewController: UIViewController {
         label1.text = task1
         label2.text = task2
         
-        view.addSubview(lineChartView)
+        createChart()
         
     }
-    
+    private func createChart() {
+        //  Create bar chart
+        let barChart = BarChartView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.width))
+        
+        // Configure the axis
+        let xAxis = barChart.xAxis
+        let rightAxis = barChart.rightAxis
+        
+        // Configure legend
+        let legend = barChart.legend
+        
+        // Suply data
+        var entries = [BarChartDataEntry]()
+        for x in 0..<10 {
+            entries.append(
+                BarChartDataEntry(
+                    x: Double(x),
+                    y: Double.random(in: 0...30)
+                )
+            )
+        }
+        let set = BarChartDataSet(entries: entries, label: "Cost")
+        set.colors = ChartColorTemplates.joyful()
+        let data = BarChartData(dataSet: set)
+        
+        barChart.data = data
+        
+        view.addSubview(barChart)
+        barChart.center = view.center
+    }
 
 
 }
